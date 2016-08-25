f43b9b5dbbf76b93faac72987d40d744fb1dfbbf
Anil Thomas <anil@nervanasys.com>
2016-08-25 07:50:12 -0700

    Change comparison test to use aeon

--- loader/test/compare.py
+++ loader/test/compare.py
@@ -16,16 +16,16 @@
 Verify that different ways of loading datasets lead to the same result.
 
 This test utility accepts the same command line parameters as neon. It
-downloads the CIFAR-10 dataset and saves it as individual JPEG files. It then
+downloads the CIFAR-10 dataset and saves it as individual PNG files. It then
 proceeds to fit and evaluate a model using two different ways of loading the
 data. Macrobatches are written to disk as needed.
 
 run as follows:
-python compare.py -e1 -r0 -bcpu -w <place where data lives>
+python compare.py -e1 -r0 -w <place where data lives>
 
 """
-import os
 import numpy as np
+from neon import NervanaObject
 from neon.data import ArrayIterator
 from neon.initializers import Uniform
 from neon.layers import Affine, Conv, Pooling, GeneralizedCost
@@ -34,77 +34,75 @@ from neon.optimizers import GradientDescentMomentum
 from neon.transforms import Misclassification, Rectlin, Softmax, CrossEntropyMulti
 from neon.callbacks.callbacks import Callbacks
 from neon.util.argparser import NeonArgparser
-from neon.data import load_cifar10
-from neon.data import ImageLoader, DataLoader, ImageParams
+from neon.util.persist import get_data_cache_dir
+from neon.data.dataloader_transformers import OneHot, TypeCast, ImageMeanSubtract
+from ingesters import ingest_cifar10
+from aeon import DataLoader
 from PIL import Image
-from glob import glob
-
-traindir = 'train'
-testdir = 'test'
 
 
-def process_dataset(data, labels, inputpath, leafdir):
-    datadir = os.path.join(inputpath, leafdir)
-    print('Saving images to %s' % datadir)
-    os.mkdir(datadir)
-    ulabels = np.unique(labels)
-    for ulabel in ulabels:
-        os.mkdir(os.path.join(datadir, str(ulabel)))
-    for idx in range(data.shape[0]):
-        im = data[idx].reshape((3, 32, 32))
-        im = np.uint8(np.transpose(im, axes=[1, 2, 0]).copy())
-        im = Image.fromarray(im)
-        path = os.path.join(datadir, str(labels[idx][0]), str(idx) + '.png')
-        im.save(path, format='PNG')
+bgr_means = [127, 119, 104]
 
 
-def process(inputpath):
-    (X_train, y_train), (X_test, y_test), nclass = load_cifar10(inputpath,
-                                                                normalize=False)
-    process_dataset(X_train, y_train, inputpath, traindir)
-    process_dataset(X_test, y_test, inputpath, testdir)
+def make_aeon_config(manifest_filename, cache_directory, minibatch_size, do_randomize=False):
+    image_decode_cfg = dict(
+        height=32, width=32,
+        scale=[1.0, 1.0],
+        flip_enable=do_randomize,
+        center=(not do_randomize))
+
+    return dict(
+        manifest_filename=manifest_filename,
+        minibatch_size=minibatch_size,
+        macrobatch_size=5000,
+        cache_directory=cache_directory,
+        subset_fraction=1.0,
+        shuffle_manifest=do_randomize,
+        shuffle_every_epoch=do_randomize,
+        type='image,label',
+        label={'binary': False},
+        image=image_decode_cfg)
 
 
-def load_dataset(basepath, datadir, shuffle):
-    path = os.path.join(basepath, datadir)
-    if not os.path.exists(path):
-        process(basepath)
-    subdirs = glob(os.path.join(path, '*'))
-    labelnames = sorted(map(lambda x: os.path.basename(x), subdirs))
-    inds = range(len(labelnames))
-    labeldict = {key: val for key, val in zip(labelnames, inds)}
-    lines = []
-    for subdir in subdirs:
-        subdirlabel = labeldict[os.path.basename(subdir)]
-        files = glob(os.path.join(subdir, '*.png'))
-        lines += [(filename, subdirlabel) for filename in files]
-    assert(len(lines) > 0)
+def transformers(dl):
+    dl = OneHot(dl, nclasses=10, index=1)
+    dl = TypeCast(dl, index=0, dtype=np.float32)
+    dl = ImageMeanSubtract(dl, index=0, pixel_mean=bgr_means)
+    return dl
+
+
+def load_dataset(basepath, datadir, manifest):
+    with open(manifest) as fd:
+        lines = fd.readlines()
+    assert len(lines) > 0, 'could not read %s' % manifest
+
     data = None
-    if shuffle:
-        np.random.seed(0)
-        np.random.shuffle(lines)
-    for idx in range(len(lines)):
-        # Convert from RGB to BGR to be consistent with the generic data loader
-        im = np.asarray(Image.open(lines[idx][0]))[:, :, ::-1]
+    for idx, line in enumerate(lines):
+        imgfile, labelfile = line.split(',')
+        labelfile = labelfile[:-1]
+        # Convert from RGB to BGR to be consistent with the data loader
+        im = np.asarray(Image.open(imgfile))[:, :, ::-1]
+        # Convert from HWC to CHW
         im = np.transpose(im, axes=[2, 0, 1]).ravel()
         if data is None:
             data = np.empty((len(lines), im.shape[0]), dtype='float32')
             labels = np.empty((len(lines), 1), dtype='int32')
         data[idx] = im
-        labels[idx] = lines[idx][1]
+        with open(labelfile) as fd:
+            labels[idx] = int(fd.read())
     data_view = data.reshape((data.shape[0], 3, -1))
     # Subtract mean values of B, G, R
-    data_view -= np.array((127, 119, 104)).reshape((1, 3, 1))
+    data_view -= np.array(bgr_means).reshape((1, 3, 1))
     return (data, labels)
 
 
 def load_cifar10_imgs(path):
-    (X_train, y_train) = load_dataset(path, traindir, shuffle=False)
-    (X_test, y_test) = load_dataset(path, testdir, shuffle=False)
+    (X_train, y_train) = load_dataset(path, 'train', train_manifest)
+    (X_test, y_test) = load_dataset(path, 'val', val_manifest)
     return (X_train, y_train), (X_test, y_test), 10
 
 
-def run(args, train, test):
+def run(train, test):
     init_uni = Uniform(low=-0.1, high=0.1)
     opt_gdm = GradientDescentMomentum(learning_rate=0.01,
                                       momentum_coef=0.9,
@@ -125,31 +123,33 @@ def run(args, train, test):
 
 
 def test_iterator():
-    print('Testing iterator based data loader')
-    parser = NeonArgparser(__doc__)
-    args = parser.parse_args()
-    (X_train, y_train), (X_test, y_test), nclass = load_cifar10_imgs(path=args.data_dir)
+    print('Testing data iterator')
+    (X_train, y_train), (X_test, y_test), nclass = load_cifar10_imgs(path=image_dir)
     train = ArrayIterator(X_train, y_train, nclass=nclass, lshape=(3, 32, 32))
     test = ArrayIterator(X_test, y_test, nclass=nclass, lshape=(3, 32, 32))
-    return run(args, train, test)
+    return run(train, test)
 
 
 def test_loader():
-    print('Testing generic data loader')
-    parser = NeonArgparser(__doc__)
-    args = parser.parse_args()
+    print('Testing data loader')
+    train_config = make_aeon_config(train_manifest, cache_dir, args.batch_size)
+    val_config = make_aeon_config(val_manifest, cache_dir, args.batch_size)
 
-    train_path = os.path.join(args.data_dir, traindir + '-ingested')
-    test_path = os.path.join(args.data_dir, testdir + '-ingested')
+    train = transformers(DataLoader(train_config, NervanaObject.be))
+    test = transformers(DataLoader(val_config, NervanaObject.be))
 
-    params = ImageParams(channel_count=3, height=32, width=32)
-    common = dict(media_params=params, target_size=1, nclasses=10)
-    train = DataLoader('train', repo_dir=os.path.join(args.data_dir, 'train'),
-                       **common)
-    test = DataLoader('test', repo_dir=os.path.join(args.data_dir, 'test'),
-                      **common)
-    err = run(args, train, test)
+    err = run(train, test)
     return err
 
 
-assert test_iterator() == test_loader()
+parser = NeonArgparser(__doc__)
+args = parser.parse_args()
+
+image_dir = get_data_cache_dir(args.data_dir, subdir='extracted')
+cache_dir = get_data_cache_dir(args.data_dir, subdir='cache')
+
+# Perform ingest if it hasn't already been done and return manifest files
+train_manifest, val_manifest = ingest_cifar10(out_dir=image_dir, padded_size=32)
+
+assert test_iterator() == test_loader(), 'The results do not match'
+print 'OK'
